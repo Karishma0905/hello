c98bf652ec4e8dcce633ad29f21cc67969c44804
Karishma <karishmakousar09@gmail.com>
2024-01-02 10:59:10 +0530

    first commit

---
+++ src/main/java/com/example/demo/Day25awsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day25awsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day25awsApplication.class, args);
+	}
+
+}
